00a26b9357e26822e4de2fe6832c8f32f55cd528
Aline Vianna <alinealvesvianna@gmail.com>
2016-12-19 17:59:45 -0200

    colocando seleção de usuários na tela, com retângulo de seleção.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var morreu = false;
-
+var inicioJogo = true;
 //Funções Utilitárias
 function numeroAleatorio(max, min) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -9,13 +9,13 @@ function numeroAleatorio(max, min) {
 function desenharRetangulo(x, y, w, h, texto, transparencia) {
     //seta parâmetros do retângulo
     var ParametrosRetangulo = {
-            retanguloStrokeStyle: "black",
-            retanguloLineWidth: 6,
-            familiaFonte: "12 px Arial",
-            corFonte: "white",
-            corRetangulo: "yellow"
-        }
-        // draw rectangular
+        retanguloStrokeStyle: "black",
+        retanguloLineWidth: 6,
+        familiaFonte: "12 px Arial",
+        corFonte: "white",
+        corRetangulo: "yellow"
+    }
+    // draw rectangular
     ctx.strokeStyle = ParametrosRetangulo.retanguloStrokeStyle;
     ctx.lineWidth = ParametrosRetangulo.retanguloLineWidth;
     ctx.globalAlpha = transparencia;
@@ -39,7 +39,7 @@ function desenharRetangulo(x, y, w, h, texto, transparencia) {
 }
 
 //Criando uma superclasse para os Personagens e suas propriedades em comum
-var Personagens = function(x, y, vidas, nivel) {
+var Personagens = function (x, y, vidas, nivel) {
     this.sprite = "sprite";
     this.x = x;
     this.y = y;
@@ -48,7 +48,7 @@ var Personagens = function(x, y, vidas, nivel) {
 }
 
 // Desenha todos os Personagens (Enemy, Player e Vidas ) na tela
-Personagens.prototype.render = function() {
+Personagens.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83 - 30);
     ctx.font = "20px Trebuchet MS";
     ctx.fillStyle = "white"
@@ -75,46 +75,68 @@ Personagens.prototype.render = function() {
     ctx.fillText(player.nivel + "°" + " Nível", 10, 90);
 
     if (morreu === true) {
-      player.x=2;
-      player.y=5;
-      desenharRetangulo(102, 150, 300, 100, "Você Perdeu, seu ruim!!", 0.2);
-      desenharRetangulo(102, 300, 300, 50, "Pressione enter para começar novamente", 0.2);
+        player.x = 2;
+        player.y = 5;
+        desenharRetangulo(102, 150, 300, 100, "Você Perdeu, seu ruim!!", 0.2);
+        desenharRetangulo(102, 300, 300, 50, "Pressione enter para começar novamente", 0.2);
     }
+
+    player.selecionarPlayer()
 
 };
 
 //Definindo metódo de uptade padrão
-Personagens.prototype.update = function() {};
+Personagens.prototype.update = function () { };
 
-Personagens.prototype.selecionarPlayer = function (sprite){
-  var players = [
-    {
-      imagemJogador : "images/char-boy.png"
-    },
-    {
-      imagemJogador : "images/char-cat-girl.png",
-    },
-    {
-      imagemJogador : "images/char-horn-girl.png",
-    },
-    {
-      imagemJogador : "images/char-pink-girl.png",
-    },
-    {
-      imagemJogador : "images/char-princess-girl.png"
+Personagens.prototype.selecionarPlayer = function (sprite) {
+    var players = [
+      {
+          imagemJogador: "images/char-boy.png",
+          altura: 88,
+          largura: 67
+      },
+      {
+          imagemJogador: "images/char-cat-girl.png",
+          altura: 90,
+          largura: 68
+
+      },
+      {
+          imagemJogador: "images/char-horn-girl.png",
+          altura: 90,
+          largura: 77
+
+      },
+      {
+          imagemJogador: "images/char-pink-girl.png",
+          altura: 89,
+          largura: 76
+
+      },
+      {
+          imagemJogador: "images/char-princess-girl.png",
+          altura: 99,
+          largura: 75
+      }
+    ]
+
+
+    for (var i = 0; i < players.length; ++i) {
+        // console.log(players)
+        var posicaoJogador = 95 * i;
+        ctx.drawImage(Resources.get(players[i].imagemJogador), 0 + posicaoJogador, 200, (players[i].largura) * 1.5, (players[i].altura) * 1.5);
     }
-  ]
 
-players.forEach(function(player){
-  var posicaoJogador = 30 * i;
-  ctx.drawImage(Resources.get(player.imagemJogador), 100 + posicaoJogador, 50, 30, 50);
-});
+
+        if(inicioJogo === true){
+          desenharRetangulo(95,  230, 100, 100, "", 0.2)
+        }
 
 
 };
 
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+var Enemy = function (x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -144,7 +166,7 @@ Enemy.prototype.constructor = Enemy;
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -156,16 +178,16 @@ Enemy.prototype.update = function(dt) {
     }
 };
 
-Enemy.prototype.morrer = function() {
+Enemy.prototype.morrer = function () {
     // allEnemies.forEach(function(enemy) {
-        enemy.speed = numeroAleatorio(5, 1);
-        enemy.x = 1;
+    enemy.speed = numeroAleatorio(5, 1);
+    enemy.x = 1;
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(x, y, vidas, nivel) {
+var Player = function (x, y, vidas, nivel) {
     Personagens.call(this);
     this.sprite = 'images/char-boy.png';
     this.x = x;
@@ -178,7 +200,7 @@ Player.prototype = new Personagens();
 Player.prototype.constructor = Player;
 
 
-Player.prototype.morrer = function() {
+Player.prototype.morrer = function () {
 
     // function desenharRetangulo(x, y, w, h, texto, transparencia)
     player.x = 2;
@@ -188,7 +210,7 @@ Player.prototype.morrer = function() {
 
 };
 
-Player.prototype.handleInput = function(key) {
+Player.prototype.handleInput = function (key) {
     // if (morreu === false) {
     switch (key) {
         case 'left':
@@ -205,7 +227,7 @@ Player.prototype.handleInput = function(key) {
                 this.nivel++;
                 this.y = 5;
 
-                allEnemies.forEach(function(enemy) { // aumenta a velocidade do inimigo
+                allEnemies.forEach(function (enemy) { // aumenta a velocidade do inimigo
                     enemy.speed += 1;
                 });
             }
@@ -227,11 +249,11 @@ Player.prototype.handleInput = function(key) {
             if (morreu === true) {
                 console.log("morri porra!")
                 player.morrer()
-                allEnemies.forEach(function(enemy) {
+                allEnemies.forEach(function (enemy) {
                     enemy.morrer()
                 })
-                premiacaoVidas.forEach(function(vida){
-                  vida.morrer();
+                premiacaoVidas.forEach(function (vida) {
+                    vida.morrer();
                 })
                 morreu = false;
             }
@@ -244,7 +266,7 @@ Player.prototype.handleInput = function(key) {
 };
 
 // Classe para instanciar vidas ao jogador
-var Vida = function(x, y, speed) {
+var Vida = function (x, y, speed) {
     Personagens.call(this);
     this.speed = speed;
     this.x = x;
@@ -256,7 +278,7 @@ Vida.prototype = new Personagens();
 Vida.prototype.constructor = Vida;
 
 
-Vida.prototype.update = function(dt) {
+Vida.prototype.update = function (dt) {
     this.x += this.speed * dt;
     if (this.x > 30) {
         this.x = -1;
@@ -267,13 +289,13 @@ Vida.prototype.update = function(dt) {
     }
 }
 
-Vida.prototype.render = function() {
+Vida.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83 - 10);
 }
 
-Vida.prototype.morrer = function(){
-  vida.y = numeroAleatorio(3, 1);
-  vida.speed = numeroAleatorio(10, 5);
+Vida.prototype.morrer = function () {
+    vida.y = numeroAleatorio(3, 1);
+    vida.speed = numeroAleatorio(10, 5);
 }
 
 // Now instantiate your objects.
@@ -299,7 +321,7 @@ for (var i = 0; i < 2; i++) {
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
